7cb254ec9a4828834ce413f0931d59ca568c538f
Fatih GÜRSOY <fg@users.noreply.github.com>
2019-08-07 17:25:37 +0300

    Update reducers.js

--- resources/js/store/reducers.js
+++ resources/js/store/reducers.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux'
 
-import auth from '../modules/auth/store/reduer'
+import auth from '../modules/auth/store/reducer'
 import user from '../modules/user/store/reducer'
 import articles from '../modules/article/store/reducer'
 
